801351781325ea1cd8129a79b565c33d8fe84b84
Rasathurai Karan <73582838+rasathuraikaran@users.noreply.github.com>
2023-03-14 01:30:38 +0530

    api function created

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_chatgpt_app/constant/constant.dart';
 import 'package:flutter_chatgpt_app/model/ChatMessage.dart';
 import 'package:flutter_chatgpt_app/widget/chatmessage.dart';
 import 'package:http/http.dart' as http;
@@ -42,7 +43,30 @@ class _ChatPageState extends State<ChatPage> {
     super.initState();
     isLoading = false;
   }
+Future<String>  generateResponse( String prompt) async {
+  final apikey=apiSecretKey;
+  var url=Uri.https("api.openai.com","/v1/completions ");
+  final response=await http.post(url,headers: {
+    'Content-Type': 'application/json' ,
+'Authorization':'Bearer $apikey' 
+  },
+  body: jsonEncode(
 
+   {
+      "model": "text-davinci-003",
+     "prompt": prompt,
+      "max_tokens": 2000,
+      "top_p": 1,
+       "temperature": 0,
+   }
+  )
+  );
+
+  Map<String,dynamic> newresponse=jsonDecode(response.body);
+
+  return newresponse['choices'][0]["text"];
+
+}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
